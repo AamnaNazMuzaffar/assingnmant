f22675d73fd6ccbd48ab7bae51da75464c13053d
AamnaNazMuzaffar <aamnanazmuzaffar@gmail.com>
2023-10-07 17:02:23 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,629 @@
+"use strict";
+//    CHAPTER NO 01    //
+//        ALERTS       //
+
+// alert("Error! please enter a vaild password")
+
+// alert(`   welcom to js land
+//    Happy Coding!`)
+
+//    alert("   welcom to js land ")
+
+// alert("Hello... I can run js through my web browser's console")
+
+//    CHAPTER NO 02    //
+//      VARIABLES FOR STRINGS    //
+
+// const username = "";
+
+// const myName = "Aamna Naz";
+
+// const js = "Hello World";
+
+// alert(js);
+
+// const Name = "Jhone Doe";
+// alert(Name);
+// const year = "15 year old";
+// alert(year);
+// const education = "Certified Mobile Appplication Development ";
+// alert(education);
+
+// alert(`
+//            PIZZA
+//            PIZZ
+//            PIZ
+//            PI
+//            P`);
+
+// alert("My email address is example@example.com");
+
+// alert("I am trying to learn from the Book A smarter way to learn javascript ");
+
+// document.querySelector(".write").textContent= "Yah! I can write HTML content thruogh javascript"
+
+// const  string = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+// alert(string)
+
+//    CHAPTER NO 03    //
+//      VARIABLES FOR NUMBERS   //
+
+// const age = "I am 15 year old"
+// alert(age)
+
+// const num = ("14");
+
+// alert(`You have visited this site ${num} times `)
+
+// const  birthYear= (`my birth year is 2006
+// date type of my declared variable is number`);
+
+// document.querySelector(".p").textContent=(birthYear)
+
+//  const name = "jonh ";
+// const order = "5 T-shirt ";
+// const store = "xyz"
+
+//  document.querySelector('h1').textContent = `${name} ordered ${order}(s) on XYZ Clothing store`
+
+//    CHAPTER NO 04    //
+//    VARIABLE NAMES: LEGAL & ILLEGAL  //
+
+// const x = 5;
+// const y = 6;
+// const z = x + y;
+
+// const amna = ("");
+// const $abc = ("");
+// const ainx12 = ("");
+
+// const new = ("");
+// const 123 = ("");
+// const !@#%^&* = ("");
+
+// const a = (`a) A heading stating “Rules for naming JS variables”
+// b) Variable names can only contain ______, ______,
+// ______ and ______.
+// For example $my_1stVariable
+// c) Variables must begin with a ______, ______ or
+// _____. For example $name, _name or name
+// d) Variable names are case _________
+// e) Variable names should not be JS _________
+// `)
+
+//    CHAPTER NO 05   //
+//    math expression  //
+
+// document.querySelector('p').textContent=(a);
+
+// const a =3+5;
+
+// document.querySelector('h1').textContent=("sum of 3 and 5 of " + a);
+
+// const b =7-5;
+
+// document.querySelector('h1').textContent=("sum of 7 and 5 of " + b);
+
+// const c =3/5;
+
+// document.querySelector('h1').textContent=("sum of 3 and 5 of " + c);
+
+// const d =3*5;
+
+// document.querySelector('h1').textContent=("sum of 3 and 5 of " + d);
+
+// const e =3%5;
+
+// document.querySelector('h1').textContent=("sum of 3 and 5 of " + e);
+
+// let num ;
+// document.querySelector('.h1').textContent=("Value after variable declaration  is: ?? " );
+
+//  num = 5;
+
+// document.querySelector('.h2').textContent=("Initial value:" + num);
+
+// num++;
+// document.querySelector('.h3').textContent=("Value after  increment  is: "  + num);
+
+// num += 7;
+// document.querySelector('.h4').textContent=("Value after  increment  is: "  + num);
+
+// num--;
+// document.querySelector('.h5').textContent=("Value after  decrement  is: "  + num);
+
+// const  ticket=600 * 5;
+// document.querySelector('.h1').textContent=(" Total cost to by tickets to a movie is "+ ticket+ " PKR");
+
+// document.write(
+//   "table of 4 <br/> " +
+//     "4 x 1 = " +
+//     4 * 1 +
+//     "<br/> 4 x 1 = " +
+//     4 * 2 +
+//     "<br/> 4 x 1 = " +
+//     4 * 3 +
+//     "<br/> 4 x 1 = " +
+//     4 * 4 +
+//     "<br/> 4 x 1 = " +
+//     4 * 5 +
+//     "<br/> 4 x 1 = " +
+//     4 * 6 +
+//     "<br/> 4 x 1 = " +
+//     4 * 7 +
+//     "<br/> 4 x 1 = " +
+//     4 * 8 +
+//     "<br/> 4 x 1 = " +
+//     4 * 9 +
+//     "<br/> 4 x 1 = " +
+//     4 * 10
+// );
+
+//    CHAPTER NO 06/09    //
+//    math expression  //
+
+// let a = 10;
+// document.write(
+//   " <h2> Result: <br> the result of a is: " +
+//     a +
+//     "<br> ..................................."
+// );
+// a++;
+
+// document.write(
+//   "<br> the value of ++a is :" +
+//     a +
+//     "<br> now the value of a is: " +
+//     a +
+//     "<br/>"
+// );
+// document.write("<br> the value of ++a is :" + a);
+// a++;
+
+// document.write("<br> now the value of a is: " + a + "<br/>");
+
+// a--;
+// document.write("<br> the value of ++a is :" + a);
+
+// document.write("<br> now the value of a is: " + a + "<br/>");
+
+// document.write("<br> the value of ++a is :" + a);
+// a--;
+
+// document.write("<br> now the value of a is: " + a + "<br/>");
+
+// const name = prompt("name");
+// document.write("<h1>WELLCOME "+ name )
+
+// const number = prompt("number");
+
+// document.write(
+//     "table of 4 <br/> " +
+//     number +
+//     " x 1 = " +
+//     number * 1 +
+//     "<br/> " +
+//     number +
+//     " x 2 = " +
+//     number * 2 +
+//     "<br/> " +
+//     number +
+//     " x 3 = " +
+//     number * 3 +
+//     "<br/> " +
+//     number +
+//     " x 4 = " +
+//     number * 4 +
+//     "<br/> " +
+//     number +
+//     " x 5 = " +
+//     number * 6 +
+//     "<br/> " +
+//     number +
+//     " x 6 = " +
+//     number * 6 +
+//     "<br/> " +
+//     number +
+//     " x 7 = " +
+//     number * 7 +
+//     "<br/> " +
+//     number +
+//     " x 8 = " +
+//     number * 8 +
+//     "<br/> " +
+//     number +
+//     " x 9 = " +
+//     number * 9 +
+//     "<br/> " +
+//     number +
+//     " x 10 = " +
+//     number * 10
+// );
+
+//    CHAPTER NO 09/11    //
+//    user input & conditional statment  //
+
+// const city = prompt("city");
+// if(city == "karachi"){
+//     document.write("wellcome to city of light ")
+// }
+
+// const gander =prompt("gender")
+// if(
+//     gander == "male"
+// ){
+//     document.write("good morning Sir")
+// }
+// else if(
+//     gander == "female"
+// ){
+//     document.write("good morning Ma'am")
+// }else{
+//     document.write("good morning")
+// }
+
+// const signale = prompt("signale color")
+
+// if(signale == "red"){
+//     document.write("<h1>must stop")
+
+// }
+// else if(signale == "yellow"){
+//     document.write("<h1>ready to move")
+
+// }
+//  else if(signale == "green"){
+//     document.write("<h1>move now")
+
+// }
+
+// const fuel = Number(prompt("fuel"));
+// if (fuel <= 25) {
+//   document.write("<h1>please refill the fuel in your car");
+// } else {
+//   document.write("<h1>you can drive");
+// }
+
+// const total = 300;
+// const obtain = 240;
+
+// const persentage = (obtain / total) * 100;
+// if (persentage >= 80) {
+//   document.write(
+//     "<h1>RESULT SHEET <h1><br> <h2>Totel Marks : " +
+//       total +"<br>"+
+//       "Marks obtained : " +
+//       obtain +"<br>"+
+//       " Percentage : " +
+//       persentage +"<br>"+
+//       "Grade : A-one" +"<br>"+
+//       "Remarks : Excellent"
+//   );
+// } else if (persentage >= 70) {
+//   document.write(
+//     "<h1>RESULT SHEET <h1><br> <h2>Totel Marks : " +
+//       total +"<br>"+
+//       "Marks obtained : " +
+//       obtain +"<br>"+
+//       " Percentage : " +
+//       persentage +"<br>"+
+//       "Grade : A" +"<br>"+
+//       "Remarks : good"
+//   );
+// } else if (persentage >= 60) {
+//   document.write(
+//     "<h1>RESULT SHEET <h1><br> <h2>Totel Marks : " +
+//       total +"<br>"+
+//       "Marks obtained : " +
+//       obtain +
+//       "<br> Percentage : " +
+//            persentage +
+//       "Grade : B" +"<br>"+
+//       "Remarks : You need to improve"
+//   );
+// } else if (persentage < 60) {
+//   document.write(
+//     "<h1>RESULT SHEET <h1><br> <h2>Totel Marks : " +
+//       total +"<br>"+
+//       "Marks obtained : " +
+//       obtain +"<br>"+
+//       " Percentage : " +
+//       persentage +"<br>"+
+//       "Grade : fail" +"<br>"+
+//       "Remarks : sorry"
+//   );}
+
+// const a =Math.trunc(Math.random()* 10 +1)
+// console.log(a)
+
+// const guss =prompt("gusse")
+
+// if(guss == a){
+//     document.write("<h1> write")
+// }else{
+//     document.write("<h1> wrong3")
+
+// }
+
+//    CHAPTER NO 14-16   //
+//    ARRAYS //
+
+// const students1 = ["mical","JHON","tiny"]
+// const students2 = [320,230,480 ]
+// const arr1 =String([])
+// const arr2 =Number([])
+// const arr3 =Boolean([])
+// const arr4 =[]
+// const arr5 =["SSC", "HSC", "BCS",
+//     "BS", "BCOM", "MS"," M. Phil.", "PhD"]
+
+//     const students1 = ["mhical","JHON","tiny"]
+// const students2 = [320,230,480 ]
+// document.write("<h1> score of "+students1[0]+ " is " +students2[0] + ". percentage : " +(students2[0] /500 ) * 100+"% <br>")
+// document.write("<h1> score of "+students1[1]+ " is " +students2[1] + ". percentage : " +(students2[1] /500 ) * 100+"% <br>")
+// document.write("<h1> score of "+students1[2]+ " is " +students2[2] + ". percentage : " +(students2[2] /500 ) * 100+"% <br>")
+
+// const a =prompt("color")
+
+// const color = ['red','blue','green',a]
+
+// document.write("<h1>"+color)
+
+// const a =prompt("color")
+
+// const color = ['red','blue','green']
+
+// color.push(a)
+
+// document.write("<h1>"+color)
+
+// const a =prompt("color")
+
+// const color = ['red','blue','green']
+
+// color.unshift(a)
+
+// document.write("<h1>"+color)
+
+// const color = ['red','blue','green']
+
+// color.unshift("pink","black")
+
+// document.write("<h1>"+color)
+
+// const color = ['red','blue','green']
+
+// color.shift()
+
+// document.write("<h1>"+color)
+
+// const color = ['red','blue','green']
+
+// color.pop()
+
+// document.write("<h1>"+color)
+
+// const b =prompt("index")
+// const a =prompt("color")
+// const color = ['red','blue','green']
+// color.splice(b,0,a)
+
+// document.write("<h1>"+color)
+
+// const b =prompt("index")
+// // const a =prompt("color")
+// const color = ['red','blue','green']
+// color.splice( undefined,b)
+
+// document.write("<h1>"+color)
+
+// const score1 =[320,230,480,120]
+
+// document.write( "<h1>"+  "Scores of Student : "+score1)
+
+// score1.sort()
+
+// document.write( "<h1>"+  "Ordered Scores of Student : "+score1)
+
+// const cities = ["karachi", "lahore", "islamabad", "Quetta"];
+// document.write("<h1>" + cities);
+
+// const selectedCities = [cities[2], cities[3]];
+// document.write("<h1>" + selectedCities);
+
+// const arr2= ["this","is","my","cat"]
+// document.write("<h1>"+"array<br>"+arr2)
+// const arr3 = arr2.join(" ")
+// document.write("<h1>"+"array<br>"+arr3)
+
+// const devise = ["mouse","keybored","printer","moniter"]
+
+// document.write("<h1>"+"device<br>"+devise)
+
+// document.write("<h1>"+"out :<br>"+devise[0]+"<br>out :<br>"+devise[1]+"<br>out :<br>"+devise[2]+"<br>out :<br>"+devise[3])
+
+//     17/20     //
+//     ARRAYS AND LOOP     //
+
+// const arr = [1,2,[3,4,[5,6]]]
+
+// for(let i = 1 ; i <= arr.length; i++){
+//     document.write("<h1>"+ arr)
+//     }
+
+// for(let i = 1 ; i <= 10; i++){
+// document.write("<h1>"+ i)
+// }
+
+// const a =prompt("table");
+
+// for(let i = 1 ; i <= 20 ; i++){
+// document.write("<h1>"+ a + "x "+ i +"= " +a*i )
+
+// }
+
+// const fruits = ["apple", " banana", " mango", "orange", "strawberry"];
+
+// fruits.forEach(f  => {
+// document.write("<h1>"+ f )
+
+// });
+
+// const fruits = ["apple", " banana", " mango", "orange", "strawberry"];
+
+// fruits.forEach(function (v,i) {
+//   document.write("<h1>"+"Elemant at index " + i + " is " + v + " :");
+// });
+
+// for(let i = 1 ; i <= 15 ; i++){
+//     document.write( i  +",")
+// }
+
+// for(let i = 10 ; i >= 1 ; i--){
+//     document.write( i  +",")
+//     }
+
+//     17/20     //
+//        STRING METHODS  //
+
+// const nam =prompt("name");
+// const last =prompt("last");
+// const a = nam +" "+ last
+
+// document.write("<h1>"+ a)
+
+// const nam =prompt("phone");
+
+// document.write("<h1>"+ "My favorite phone is : " + nam +" length of string : "+ nam.length)
+
+// const city = "pakistan"
+// document.write("<h1>"+ "String : "+city +"<br>" +"index of 'n '" +city.indexOf("n"))
+
+// const city = "Hello World"
+// document.write("<h1>"+ "String : "+city +"<br>" +"index of 'l '" +city.lastIndexOf("l"))
+
+// const city = "Pakistani"
+// document.write("<h1>"+ "String : "+city +"<br>" +" character at index 3 : " +city[3])
+
+// const nam =prompt("name");
+// const last =prompt("last");
+// const a = nam.concat(last)
+
+// document.write("<h1>"+ a)
+
+// const a = "hyderabad";
+// document.write("<h1>"+"city "+ a)
+// const b = a.replace( "hyderabad","islamabad" )
+// document.write("<h1>"+"after replacemant "+ b)
+
+// const message =" Ali and Sami are best friends. They play cricket and football together.";
+// document.write("<h1>"+ message)
+
+// const a = message.replaceAll("and","&")
+// document.write("<h1>"+ a)
+
+// const message ="472";
+// document.write("<h1>"+"Value "+ message+"<br>" +"Value "+ typeof message)
+
+// const a = message.replace("472", 473)
+// document.write("<h1>"+"Value "+ a+"<br>" +"Value "+ typeof a)
+
+// const a = prompt("name");
+// const b = a.toUpperCase();
+// document.write("<h1>"+ b)
+
+// const a = prompt("name");
+// const b = a.toLowerCase();
+// document.write("<h1>"+ b)
+
+// const a = prompt("user")
+// // const b =["@ ",". ",", ","!"]
+// if ( a == 33){
+//     alert("enter a valid username.")
+// }else{
+// document.write("<h1>"+ a)
+
+// }
+
+// const a = ["cake", "apple pie", "cookie", "chips", "patties"]
+// const b = prompt("fruite")
+
+// if(a.includes(b)){
+// document.write( "<h1>"+"cookie is avaliable at index "+ a.indexOf(b) +" in our bakery" )}
+// else{
+// document.write("<h1>"+ "we are sorry . "+ b +" is not available in oue bakery")
+
+// }
+
+// const a = "University of Karachi"
+
+// const b = a.split("")
+
+// document.write("<h1>"+ b)
+
+//     17/20     //
+//     MATH METHODS  //
+
+// const num =Number( prompt("Number"))
+// document.write("<h1>"+"number : " + num)
+// const a=Math.round(num)
+// document.write("<h1>"+"round off value : " +a)
+
+// const b=Math.floor(num)
+// document.write("<h1>"+"floor off value : " +b)
+
+// const d=Math.ceil(num)
+// document.write("<h1>"+"ceil off value : " +d)
+
+
+
+//////////////////////////////////////////////
+
+
+// const num =Number( prompt("Number"))
+// document.write("<h1>"+"number : " + num)
+// const a=Math.round(num)
+// document.write("<h1>"+"round off value : " +a)
+
+// const b=Math.floor(num)
+// document.write("<h1>"+"floor value : " +b)
+
+// const d=Math.ceil(num)
+// document.write("<h1>"+"ceil value : " +d)
+
+
+// const number =prompt("number")
+// const number2 = Math.abs(number)
+// document.write("<h1>"+"the absolute value of " +number +" is "+number2)
+
+
+
+// const number2 = Math.trunc(Math.random() * 6)+1
+// document.write("<h1>"+"random dice value  " + number2)
+
+
+// const coine = Math.trunc(Math.random() * 2)+1
+// if (coine == 1){
+// document.write("<h1>"+coine+"<br> ramdom coin value : Tails" )
+// }else{
+// document.write("<h1>"+coine+"<br> ramdom coin value : Heads" )
+// }
+
+
+
+// const rendom = Math.trunc(Math.random() * 100)+1
+// document.write("<h1>"+"random number between 1 and 100 : " +rendom)
+
+
+// const weight = prompt("WEIGHT")
+// document.write("<h1>"+"The weight of user is  : " +weight+" kilograms")
+
+
+// const secret = Math.trunc(Math.random() * 10)+1
+// console.log(secret)
+// const user = prompt("enter a secret number")
+// if (user == secret){
+//     document.write("<h1>" +"YOUR WIN <br> the number is " +secret)
+// }else{
+//     alert("TRY AGAIN")
+// }
